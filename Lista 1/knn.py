57226b0d2b4246a8e9a9cc00111de67e94db57d0
hcaula <henrique.caula@hotmail.com>
2018-03-24 19:14:54 -0300

    Adds arff lib

--- Lista 1/knn.py
+++ Lista 1/knn.py
@@ -1,7 +1,11 @@
-import sys
+import sys, os
+import arff
 
 k = int(sys.argv[1])
 w = sys.argv[2] in ["weighted", "w", "true"]
 distance = sys.argv[3]
 datasets = [sys.argv[4], sys.argv[5]]
 
+data = arff.load(open('datasets/cm1.arff', 'rb'))
+
+print data
