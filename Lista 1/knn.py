8c4a163231312b2a15bb25378fa8fc748693fded
hcaula <henrique.caula@hotmail.com>
2018-03-25 15:21:45 -0300

    Finishes basic k-NN structure

--- Lista 1/knn.py
+++ Lista 1/knn.py
@@ -30,24 +30,51 @@ dsize = len(dataset)
 ksize = dsize/kfold
 
 ## For each division (k-fold cross-validation)
+evaluations = []
 for i in range(kfold + 1):
     begin_index = i * ksize
     end_index = min((begin_index + ksize), dsize-1)
 
-    evaluation = dataset[begin_index: end_index]
-    training = dataset[0:begin_index] + dataset[end_index:dsize-1]
+    ## If the k-fold divides the training set perfectly, we shouldn't run the last k-fold
+    if end_index > begin_index:
+        rights = 0
 
-    test = []
-    for e in evaluation:
-        dists = []
-        tests = []
-        for t in training: 
-            dist = distance(e, t)
-            t_class = t[len(t)-1] in ['True', 'true']
+        evaluation = dataset[begin_index: end_index]
+        training = dataset[0:begin_index] + dataset[end_index:dsize-1]
 
-            obj = {'distance': dist, 'class': t_class}
-            dists.append(obj)
+        for e in evaluation:
+            dists = []
+            tests = []
+            for t in training: 
+                dist = distance(e, t)
+                t_class = t[len(t)-1] in ['True', 'true']
 
-        # Sorts the distances and gets the k-nearest neighbours
-        dists.sort(key=lambda x: x['distance'])
-        neighbours = dists[0:k]
+                obj = {'distance': dist, 'class': t_class}
+                dists.append(obj)
+
+            # Sorts the distances and gets the k-nearest neighbours
+            dists.sort(key=lambda x: x['distance'])
+
+            ## Checking if the last fold has less elements the chosen k
+            end_index = min(k, len(dists)-1)
+            neighbours = dists[0:end_index]
+
+            # Counts the number of classes for the k-nearests neighbours
+            pos = 0
+            neg = 0
+            for n in neighbours:
+                if(n['class']): pos += 1
+                else: neg += 1
+            
+            # If there are more positives than negatives, set prediction as True
+            prediction = False
+            if(pos >= neg): prediction = True
+            
+            # Checks if prediction was correct
+            
+
+            real_class = e[len(e)-1] in ['True', 'true']
+            if(real_class == prediction): rights += 1
+
+        print 'K-Fold: ' + str(i)
+        print 'Right predictions: ' + str(rights) + ' out of ' + str(len(evaluation))
