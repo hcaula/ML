849dd0b70cbbf99508070ffde459d60d5570b4e1
hcaula <henrique.caula@hotmail.com>
2018-04-01 08:59:25 -0300

    Fixes distance bug

--- Lista 1/distances.py
+++ Lista 1/distances.py
@@ -4,7 +4,8 @@ def euclidean(x, y):
     size = len(x)
     sum = 0
     for i in range(size): 
-        if(type(x[i]) is int or type(x[i]) is float): sum += pow((x[i] - y[i]), 2)
+        if((type(x[i]) is int or type(x[i]) is float) and (type(y[i]) is int or type(y[i]) is float)): 
+            sum += pow((x[i] - y[i]), 2)
     return math.sqrt(sum)
     
 
