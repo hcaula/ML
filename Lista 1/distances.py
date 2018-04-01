a2f98907c545d9b565832ee05bb0155450722018
hcaula <henrique.caula@hotmail.com>
2018-04-01 12:35:58 -0300

    Calculates probability for each class for each attribute

--- Lista 1/distances.py
+++ Lista 1/distances.py
@@ -11,25 +11,32 @@ def euclidean(x, y):
 def vdm(x, y):
     return 2
 
-partials = {}
-totals = []
+probabilities = {}
 def precalc_partials(dataset):
+    partials = {}
+    totals = []
 
-    # Gets the classes
+    # Calculates each partial sum for each class
     for d in dataset:
         c = d[len(d)-1]
         if(c not in partials): 
             partials[c] = []
+            probabilities[c] = []
         
         count = 0
         for i in d:
             if (count < len(d)-1):
-                if(len(partials[c]) <= count): partials[c].append({})
+                if(len(partials[c]) <= count): 
+                    partials[c].append({})
+                    probabilities[c].append({})
 
-                if(i not in partials[c][count]): partials[c][count][i] = 1
+                if(i not in partials[c][count]): 
+                    partials[c][count][i] = 1
+                    probabilities[c][count][i] = 0
                 else: partials[c][count][i] += 1
             count += 1
 
+    # Calculates total sum for each attribute
     for c in partials:
         count = 0
         for pos in partials[c]:
@@ -38,4 +45,15 @@ def precalc_partials(dataset):
             for attr in pos:
                 if(attr not in totals[count]): totals[count][attr] = 0
                 totals[count][attr] += partials[c][count][attr]
+            count += 1
+    
+    # Calculates probability for each class for each attribute
+    for c in partials:
+        count = 0
+        for pos in partials[c]:
+            for attr in partials[c][count]:
+                p = float(partials[c][count][attr])
+                t = float(totals[count][attr])
+                probabilities[c][count][attr] = p/t
+            
             count += 1
